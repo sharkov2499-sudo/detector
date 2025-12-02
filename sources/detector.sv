8a78d57ec349f2a63deda2e63cdcfa7629895efa
Default User <user@default.com>
2025-12-02 19:11:35 +0400

    feat: Baseline detector.sv for student starting point

---
+++ sources/detector.sv
@@ -0,0 +1,7 @@
+module detector(
+    input clk, // clock signal
+    input rst, // reset input
+    input in, // binary input
+    output reg detector_out // output of the sequence detector
+);
+endmodule
